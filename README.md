b6b31edd99553b675180bac9a097d5e1303b8130
Imad Saddik <simad3647@gmail.com>
2025-08-14 12:40:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StarFieldEffect
+
+WIP
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: "Inter", Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  width: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background: #01030e;
+}
+</style>
